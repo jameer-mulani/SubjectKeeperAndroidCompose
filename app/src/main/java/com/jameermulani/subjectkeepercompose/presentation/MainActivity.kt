a940e6996a4914ee6525c14ba5ea694fed35b5e3
jameermulani <jameer.a.mulani@gmail.com>
2024-01-24 17:43:04 +0530

    set up hilt

--- app/src/main/java/com/jameermulani/subjectkeepercompose/presentation/MainActivity.kt
+++ app/src/main/java/com/jameermulani/subjectkeepercompose/presentation/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.jameermulani.subjectkeepercompose.presentation.composable.SubjectKeeperApp
 import com.jameermulani.subjectkeepercompose.presentation.ui.theme.SubjectKeeperComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
